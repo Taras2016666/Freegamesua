7f27de47620519d2aab3e976fd4bd8210db09ea3
Taras2016666 <tarasfed2016@gmail.com>
2025-03-25 22:38:11 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM повністю завантажений і проаналізований');
-    // Тут можна додати свій JavaScript код для додаткової функціональності
 });
